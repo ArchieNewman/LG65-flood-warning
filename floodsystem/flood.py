ef702524205708fe529f2b35a38d2d4122d2a791
Emilia Keavney <ek560@cam.ac.uk>
2022-02-27 13:06:38 +0000

    task C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -22,3 +22,33 @@ def stations_level_over_threshold(stations, tol):
     #sorts list in descending order
 
     return stations_over #returns list of sorted tuples
+
+    #-------------------------------------------------------------------------------------------
+    #Task 2C
+def stations_highest_rel_level(stations, N):
+    """function that returns a list of the N stations at which the water level, relative to typical range,is highest"""
+
+    consistent_stations = [] #creates an empty list for station, relative water level tuples
+    station_level_tup = () #creates an empty station, water level tuple
+    N_highest_stations = [] #creates an empty list for the N stations with highest relative water level
+
+    for station in stations: #cycles through each station
+        if station.relative_water_level() != None: #only applies if data is consistent (there is a relative water level)
+            station_level_tup = (station, station.relative_water_level()) #makes tuple with station and relative water level
+            consistent_stations.append(station_level_tup) #adds tuple to the list
+
+    def sortsecond(tup): #sorting fuction- sort by second entry of tuple
+        return tup[1]
+
+    consistent_stations.sort(key = sortsecond, reverse=True) #sorts by relative water level in descending order 
+
+    for i in range(N):
+        N_highest_stations.append(consistent_stations[i][0]) #adds N highest relative water levels to the list
+
+
+    return N_highest_stations #returns the list
+
+
+
+
+
